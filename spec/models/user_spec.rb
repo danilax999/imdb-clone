30aa9604696a7c7e6c4017cc82ecc71735174ef8
Daniil Kharitonov <dan.haritonoff@gmail.com>
2022-09-26 23:37:04 +0200

    add user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { build :user }
+
+  describe 'validations' do
+    it { should validate_presence_of :fullname }
+    it { should validate_length_of(:fullname).is_at_least(2) }
+
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+
+    it { should validate_presence_of :password }
+  end
+end
